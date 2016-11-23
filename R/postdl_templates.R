4a88d85c2392009225b8f0feadd400de996621cf
johndharrison <johndharrison0@gmail.com>
2016-11-23 12:37:10 -0800

    Update unzip function to handle tar files and set resulting files to be executable.

--- R/postdl_templates.R
+++ R/postdl_templates.R
@@ -1,9 +1,11 @@
-#' Unzip downloaded files
+#' Unzip/Untar downloaded files
 #'
-#' Unzip downloaded files. Keeps the original zip file
+#' Unzip/Untar downloaded files. Keeps the original zip file
 #'
 #' @param dlfiles A data.frame of files by platform and indicating
 #'     whether they were processed
+#' @param chmod change the mode of the unarchived file/files to "700" so
+#'     they are executable
 #'
 #' @return Returns a list of character vectors indicating files
 #'     processed
@@ -25,20 +27,46 @@
 #' procyml
 #' }
 
-unzip_dlfiles <- function(dlfiles){
+unziptar_dlfiles <- function(dlfiles, chmod = FALSE){
   assert_that(is_data.frame(dlfiles))
+  assert_that(is_logical(chmod))
   if(nrow(dlfiles) == 0L){
     return()
   }
-  unzip_file <- function(platform, file, processed){
+  unzip_file <- function(platform, file, processed, chmod){
     if(!processed){return()}
     exdir <- dirname(file)
     utils::unzip(file, exdir = exdir)
+    if(chmod){
+      zfiles <- utils::unzip(file, exdir = exdir, list = TRUE)
+      Sys.chmod(file.path(exdir, basename(zfiles[["Name"]])), "700")
+    }
   }
-  Map(unzip_file,
+  untar_file <- function(platform, file, processed, chmod){
+    if(!processed){return()}
+    exdir <- dirname(file)
+    utils::untar(file, exdir = exdir)
+    if(chmod){
+      gzfiles <- utils::untar(file, exdir = exdir, list = TRUE)
+      Sys.chmod(file.path(exdir, basename(gzfiles)), "700")
+    }
+  }
+  unarchive <- function(platform, file, processed, chmod){
+    is_zip <- grepl("\\.zip$", file, ignore.case = TRUE)
+    is_tar <- grepl(".*\\.(tgz$)|(tar\\.gz$)", file, ignore.case = TRUE)
+    if(!any(c(is_zip, is_tar))){
+      stop(file, " does not appear to be a zip or tar file.\n")
+    }
+    func <- switch(c("zip", "tar")[c(is_zip, is_tar)],
+                   zip = unzip_file,
+                   tar = untar_file)
+    func(platform, file, processed, chmod)
+  }
+  Map(unarchive,
       platform = dlfiles[["platform"]],
       file = dlfiles[["file"]],
-      processed = dlfiles[["processed"]])
+      processed = dlfiles[["processed"]],
+      chmod = chmod)
   list(processed = dlfiles[dlfiles[["processed"]], "file"])
 }
 
