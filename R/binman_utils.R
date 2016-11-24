bfab304f2634f866eba7c617fef1b8720b4733cc
johndharrison <johndharrison0@gmail.com>
2016-11-24 06:43:39 -0800

    Add function to remove an app version by platform

--- R/binman_utils.R
+++ R/binman_utils.R
@@ -29,7 +29,8 @@ list_versions <- function(appname, platform = c("ALL")){
   platforms <- if(!identical(platform, "ALL")){
     platind <- platforms %in%platform
     if(!any(platind)){
-      stop("No platforms found for ", appname, " in ", platform)
+      stop("No platforms found for ", appname, " in ",
+           paste(platform, collapse = ","))
     }
     platforms[platind]
   }else{
@@ -42,10 +43,50 @@ list_versions <- function(appname, platform = c("ALL")){
   setNames(res, platforms)
 }
 
+#' Remove application version
+#'
+#' Remove application version for a given platform
+#'
+#' @param appname A character string giving the name of the application
+#' @param platform A character string indicating the platform.
+#' @param version A character vector of versions to remove. Defaults to
+#'     "ALL"
+#'
+#' @return Returns a logical vector indicating whether the removal of
+#'     version was successful. Return is invisible.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' appdir <- app_dir(appname, FALSE)
+#' platforms <- LETTERS[1:4]
+#' versions <- LETTERS[5:7]
+#' mkdirs <- file.path(appdir, outer(platforms, versions, file.path))
+#' chk <- vapply(mkdirs, dir.create, logical(1), recursive = TRUE)
+#' expect_true(all(chk))
+#' appver <- list_versions(appname)
+#' rm_version(appname, platforms[2], versions[1:2])
+#' unlink(appdir, recursive = TRUE)
+#' }
+
 rm_version <- function(appname, platform, version = c("ALL")){
   assert_that(is_string(appname))
-  assert_that(is_character(platform))
-  appdir <- rappdirs::user_data_dir(appname, "binman")
+  assert_that(is_string(platform))
+  appdir <- app_dir(appname)
+  versions <- list_versions(appname, platform)[[platform]]
+  versions <- if(!identical(version, "ALL")){
+    verind <- versions %in%version
+    if(!any(verind)){
+      stop("No versions found for ", appname, " in ",
+           paste(version, collapse = ","))
+    }
+    versions[verind]
+  }else{
+    versions
+  }
+  res <- vapply(file.path(appdir, platform, versions), unlink, integer(1),
+                recursive = TRUE)
+  invisible(!as.logical(res))
 }
 
 #' Get application directory
--- tests/testthat/test-binman_utils.R
+++ tests/testthat/test-binman_utils.R
@@ -30,3 +30,19 @@ test_that("canCheckBinmanAppDir", {
   expect_error(appdir <- app_dir("superduperapp"),
                "superduperapp app directory not found")
 })
+
+test_that("canRemoveBinmanAppVersion", {
+  appname <- "superduperapp"
+  appdir <- app_dir(appname, FALSE)
+  on.exit(unlink(appdir, recursive = TRUE))
+  platforms <- LETTERS[1:4]
+  versions <- LETTERS[5:7]
+  mkdirs <- file.path(appdir, outer(platforms, versions, file.path))
+  chk <- vapply(mkdirs, dir.create, logical(1), recursive = TRUE)
+  expect_true(all(chk))
+  appver <- list_versions(appname)
+  chk <- rm_version(appname, platforms[2], versions[1:2])
+  expect_true(all(chk))
+  appver <- list_versions(appname)
+  expect_identical(appver[[platforms[2]]], versions[3])
+})
