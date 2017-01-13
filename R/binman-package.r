72b7e5911962c4326b335614e8958f9d208e9721
johndharrison <johndharrison0@gmail.com>
2017-01-13 06:47:27 -0800

    Add functions from semver

--- R/binman-package.r
+++ R/binman-package.r
@@ -2,8 +2,6 @@
 #'
 #' @importFrom stats setNames
 #' @importFrom assertthat assert_that
-#' @importFrom semver parse_version render_version set_version
-#'     reset_version increment_version
 #' @name binman
 #' @docType package
 NULL
--- R/utils.R
+++ R/utils.R
@@ -40,3 +40,58 @@ get_os <- function (){
     stop("Unknown OS")
   }
 }
+
+#' Parse semantic version
+#'
+#' See \code{\link[semver]{parse_version}} for more details.
+#'
+#' @name parse_version
+#' @rdname parse_version
+#' @importFrom semver parse_version
+#' @keywords internal
+#' @export
+semver::parse_version
+
+#' Render semantic version
+#'
+#' See \code{\link[semver]{render_version}} for more details.
+#'
+#' @name render_version
+#' @rdname render_version
+#' @importFrom semver render_version
+#' @keywords internal
+#' @export
+semver::render_version
+
+#' Set semantic version
+#'
+#' See \code{\link[semver]{set_version}} for more details.
+#'
+#' @name set_version
+#' @rdname set_version
+#' @importFrom semver set_version
+#' @keywords internal
+#' @export
+semver::set_version
+
+#' Reset semantic version
+#'
+#' See \code{\link[semver]{reset_version}} for more details.
+#'
+#' @name reset_version
+#' @rdname reset_version
+#' @importFrom semver reset_version
+#' @keywords internal
+#' @export
+semver::reset_version
+
+#' Increment semantic version
+#'
+#' See \code{\link[semver]{increment_version}} for more details.
+#'
+#' @name increment_version
+#' @rdname increment_version
+#' @importFrom semver increment_version
+#' @keywords internal
+#' @export
+semver::increment_version
