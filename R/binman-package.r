efaa4d1e27ea2b721d63f0bdaa9980becf1a6ffc
johndharrison <johndharrison0@gmail.com>
2017-01-13 02:50:53 -0800

    Semantic versioning moved to seperate package

--- R/binman-package.r
+++ R/binman-package.r
@@ -2,6 +2,8 @@
 #'
 #' @importFrom stats setNames
 #' @importFrom assertthat assert_that
+#' @importFrom semver parse_version render_version set_version
+#'     reset_version increment_version
 #' @name binman
 #' @docType package
 NULL
